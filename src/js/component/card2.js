d11174bc74c42ab4ee6d23b4ca440f7228b33ced
George Mihov <gmihov001@gmail.com>
2021-10-15 18:57:35 +0000

    Updates

---
+++ src/js/component/card2.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const Card2 = props => {
+	var data = props.entity;
+
+	let propArr = [];
+	for (let property in data) {
+		propArr.push({ propname: property, propvalue: data[property] });
+	}
+	console.log("propArr", propArr);
+	return (
+		<div className="card m-3" style={{ width: "18rem" }}>
+			<img src={props.imgUrl} className="card-img-top" alt="..." />
+			<div className="card-body">
+				<h5 className="card-title">{propArr[0].propvalue}</h5>
+				<p className="card-text">
+					{propArr[4].propname}: {propArr[4].propvalue}
+					<br />
+					{propArr[6].propname}: {propArr[6].propvalue}
+					<br />
+					{propArr[7].propname}: {propArr[7].propvalue}
+				</p>
+				<div className="d-flex justify-content-between">
+					<Link
+						to={{
+							pathname: `/details/${props.index + 1}`,
+							state: props
+						}}>
+						<button href="#" className="btn btn-outline-dark learn-more">
+							LEARN MORE
+						</button>
+					</Link>
+					<Context.Consumer>
+						{({ actions, store }) => {
+							const isFav = store.favorites.find(f => f.entity.name == props.entity.name);
+							console.log("isFave", isFav);
+
+							return (
+								<button
+									type="button"
+									className="btn btn-outline-warning"
+									onClick={isFav ? null : () => actions.addToFavorites(props)}>
+									{isFav ? <i className="fas fa-heart" /> : <i className="far fa-heart" />}
+								</button>
+							);
+						}}
+					</Context.Consumer>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+Card2.propTypes = {
+	character: PropTypes.object,
+	planet: PropTypes.object,
+	entity: PropTypes.object,
+	name: PropTypes.string,
+	species: PropTypes.array,
+	gender: PropTypes.string,
+	eye_color: PropTypes.string,
+	hair_color: PropTypes.string,
+	index: PropTypes.number,
+	imgUrl: PropTypes.string
+};
