24277d1e3f1bc55c17c21d3b058b7b25ba32ff38
George Mihov <gmihov001@gmail.com>
2021-10-15 18:56:17 +0000

    Updates

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,28 +1,53 @@
-const getState = ({ getStore, getActions, setStore }) => {
+const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			favorites: []
 		},
 		actions: {
+			loadPeople: () => {
+				fetch("https://swapi.dev/api/people/")
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						console.log("data", data);
+						setStore({ characters: data.results });
+					});
+			},
+			loadPlanets: () => {
+				fetch("https://swapi.dev/api/planets/")
+					.then(response => {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						console.log("data", data);
+						setStore({ planets: data.results });
+					})
+					.catch(error => console.log(error));
+			},
+			addToFavorites: entity => {
+				var tempStore = getStore();
+				if (!tempStore.favorites.includes(entity)) {
+					tempStore.favorites.push(entity);
+					setStore({ tempStore });
+				}
+			},
+			deleteFromFavorites: elm => {
+				var { favorites } = getStore();
+
+				setStore({
+					favorites: favorites.filter(f => f.name != elm.name)
+				});
+			},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
 			},
 			changeColor: (index, color) => {
 				//get the store
